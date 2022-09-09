c9e4a04519ce8937f9e9ac0835737f68d8f9b0bd
Ren <ren.t21@icloud.com>
2022-09-09 14:35:22 +0900

    Initial Commit

---
+++ Stargazers-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stargazers-SwiftUI
+//
+//  Created by 高橋蓮 on 2022/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stargazers-SwiftUI/Stargazers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stargazers_SwiftUIApp.swift
+//  Stargazers-SwiftUI
+//
+//  Created by 高橋蓮 on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct Stargazers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
